8e3795f5d9b2de80e03bb5625bbc45be558eb8dc
SKYJ0 <kcsbomba@gmail.com>
2026-01-29 04:50:56 +0100

    Remove sensitive files (keys, secrets, test data)

--- dump_pdf.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pypdf import PdfReader
-
-try:
-    reader = PdfReader(r"C:\Users\kawka\Downloads\jalkshbdkljashlfhiosad893729814.pdf")
-    for page in reader.pages:
-        print(page.extract_text())
-except Exception as e:
-    print(e)
