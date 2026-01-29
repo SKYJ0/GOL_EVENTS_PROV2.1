d0819bfb777855b1b8541c2383770a7871caff63
SKYJ0 <kcsbomba@gmail.com>
2026-01-29 04:48:48 +0100

    Add .gitignore to exclude temporary files and test folders

--- legacy_python/compile_scripts.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import os
-
-script_files = [os.path.join("scripts", f) for f in os.listdir("scripts") if f.endswith(".py")]
-setup(ext_modules=cythonize(script_files, language_level="3"))
